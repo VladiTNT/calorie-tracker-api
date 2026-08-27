78e46c842d8eccb5060c2a0214f72cadc7766719
VladiTNT <vladnastasie2008@gmail.com>
2026-08-27 10:18:38 +0300

    meals ok

--- internal/database/queries/meal.go
+++ internal/database/queries/meal.go
@@ -18,7 +18,32 @@ func InsertMeal(e Executor, ctx context.Context, m *models.Meal) error {
 	return nil
 }
 
-func GetMealsByUserByDay(e Executor, ctx context.Context, userName string, day time.Time) ([]models.Meal, error) {
-	_ = "SELECT * FROM meal WHERE user_name = ? AND added_at"
-	return []models.Meal{}, nil
+func GetMealsOnDayByUsername(e Executor, ctx context.Context, userName string, day time.Time) ([]models.Meal, error) {
+	query := "SELECT * FROM meal WHERE user_name = ?;"
+
+	rows, err := e.QueryContext(ctx, query, userName)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var meals []models.Meal
+	for rows.Next() {
+		var m models.Meal
+		if err := rows.Scan(&m.User, &m.Name, &m.Calroies, &m.AddedAt); err != nil {
+			return nil, err
+		}
+		meals = append(meals, m)
+	}
+
+	var todayMeals []models.Meal
+
+	for _, m := range meals {
+		if m.AddedAt.After(day.Truncate(time.Hour*24)) &&
+			m.AddedAt.Before(day.Truncate(time.Hour*24).Add(time.Hour*24)) {
+			todayMeals = append(todayMeals, m)
+		}
+	}
+
+	return todayMeals, nil
 }
